2ae649f1725bcf2aaf3f8d1457b6b0e5957356f6
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-13 14:01:02 +0900

    feature：h2データベースのDDL文などを定義

--- src/main/resources/database/H2-dataload.sql
+++ src/main/resources/database/H2-dataload.sql
@@ -1,2 +1,3 @@
 /* load the records. */
+INSERT INTO account(username, password, first_name, last_name) VALUES('demo', '{pbkdf2}9cccc80b1782715d013a4db1bd33306e53fc534b5052f9b5ff7f50062f3d6df8d4f3395639686016e5eb803639ca1d10', 'Taro', 'Yamada');
 commit;
